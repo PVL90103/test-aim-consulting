7a09f6a6f2f89b061d1b5175ecb07554018e1bd0
pshen <pshenpavel2015@gmail.com>
2020-09-07 00:48:22 +0600

    add printer

--- src/main/java/printer/Printer.java
+++ src/main/java/printer/Printer.java
@@ -3,26 +3,61 @@ package printer;
 import executor.Executor;
 import reader.CSVReader;
 
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
 public class Printer {
     public static void print() {
 
+        Scanner in = new Scanner(System.in);
+        String answer = "";
+        List<String> files = new ArrayList<>();
 
-        String fileName1 = "src/main/resources/input1.csv";
-        String fileName2 = "src/main/resources/input2.csv";
-        String fileName3 = "src/main/resources/biostats.csv";
+        while (!answer.equalsIgnoreCase("ok")) {
+            System.out.println("Your selected files: " );
+            files.forEach(System.out::println);
+            System.out.println("========================================================");
+            System.out.println("Write the full path of your file, or write ok to cancel");
+            System.out.println("-->");
+            answer = in.nextLine();
 
-        CSVReader csvReader1 = new CSVReader(fileName1);
-        CSVReader csvReader2 = new CSVReader(fileName2);
-        CSVReader csvReader3 = new CSVReader(fileName3);
+            File file = new File(answer);
+            if (!answer.equalsIgnoreCase("ok")) {
+                if (!file.exists() || file.isDirectory()) {
+                    System.out.println("File not found");
+                }
+                if (file.exists() && !file.isDirectory()) files.add(answer);
+            }
+            //clear the screen
+            System.out.flush();
+        }
 
-
-        Executor executor1 = new Executor(csvReader1);
-        Executor executor2 = new Executor(csvReader2);
-        Executor executor3 = new Executor(csvReader3);
-
-        executor1.start();
-        executor2.start();
-        executor3.start();
+        if (!files.isEmpty()) {
+            for(String item : files) {
+                CSVReader csvReader = new CSVReader(item);
+                Executor executor = new Executor(csvReader);
+                executor.start();
+            }
+        }
+//        String fileName1 = "src/main/resources/input1.csv";
+//        String fileName2 = "src/main/resources/input2.csv";
+//        String fileName3 = "src/main/resources/biostats.csv";
+//
+//        CSVReader csvReader1 = new CSVReader(fileName1);
+//        CSVReader csvReader2 = new CSVReader(fileName2);
+//        CSVReader csvReader3 = new CSVReader(fileName3);
+//
+//
+//        Executor executor1 = new Executor(csvReader1);
+//        Executor executor2 = new Executor(csvReader2);
+//        Executor executor3 = new Executor(csvReader3);
+//
+//        executor1.start();
+//        executor2.start();
+//        executor3.start();
 
     }
 }
